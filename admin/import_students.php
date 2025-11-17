140700f5eeb729377844eb7ad40b1259e306f0c7
muskan628 <muskanbhagria94@gmail.com>
2025-11-17 14:22:16 +0530

    changes

--- admin/import_students.php
+++ admin/import_students.php
@@ -50,17 +50,8 @@ if (isset($_FILES['csv_file']['name'])) {
 
         // Build query
         $sql = "INSERT INTO students (
-            s_no, date_of_admission, tid, department, program, current_class, auid,
-            student_name, father_name, mother_name, gender, date_of_birth, category,
-            sub_category, religion, facility_availed, hosteller, transport, address,
-            city, district, state, pin_code, nationality, mobile_number, email,
-            aadhar_number, father_mobile, mother_mobile, guardian_mobile, blood_group,
-            quota, matric_percentage, senior_secondary_percentage, ug_percentage,
-            pg_percentage, fee_concession, scholarship, remarks, matriculation_certificate,
-            migration_certificate, plus2_certificate, copy_all_dmcs, character_certificate,
-            aadhar_self, photographs, study_gap_certificate, undertakings, caste_certificate,
-            bank_account_copy, income_certificate, aadhar_father, aadhar_mother,
-            aadhar_guardian, residence_proof
+           s_no ,
+    date_of_admission,tid,department ,program ,current_class ,auid,student_name ,father_name ,mother_name ,gender ,date_of_birth ,category ,sub_category ,religion ,facility_availed ,village V,student_contact ,parents_contact ,stu_email ,father_occupation ,annual_income ,city ,district ,state ,pincode ,student_aadhar ,father_aadhar ,mother_aadhar ,guardian_relation ,guardian_name ,school_12th_name ,board_12th ,pass_year_12th ,marks_12th ,graduation_college ,graduation_university ,graduation_year ,graduation_marks ,status ,future_student ,admission_session ,nationality ,startup_class ,rural_urban ,permanent_address ,reference ,matriculation_certificate ,migration_certificate ,certificate_12th ,copy_of_all_dmcs ,character_certificate ,copy_aadhar_card_self ,study_gap_certificate ,undertakings ,caste_certificate ,bank_account_copy ,income_certificate ,aadhar_card_father ,aadhar_card_mother ,aadhar_card_guardian ,residence_pro
         ) VALUES (" . str_repeat("?,", 55);
         
         $sql = rtrim($sql, ",") . ")";
